f1f654e3248c40ad3cbeb4b47150dd575c4b4cc1
matbout <48333440+matbout@users.noreply.github.com>
2024-06-04 23:38:02 -0400

    Add test function in exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,19 @@
 #[macro_use]
 extern crate napi_derive;
 
+use std::fs;
+
+use contract::Contract;
+
+mod contract;
+mod vm;
+
+const CONTRACT_PATH: &str = "resources/release.wasm";
+
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub fn test(contract_address: String, deployer_address: String) {
+    let contract_bytecode = fs::read(CONTRACT_PATH).expect("Unable to read contract file");
+    let mut contract = Contract::new(contract_bytecode, &contract_address, &deployer_address);
+    contract.init();
+    contract.call("getContract", &[]);
 }
