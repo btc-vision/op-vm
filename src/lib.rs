ff5c233243a1c27f7510a9e84a77b177bcd5f782
matbout <48333440+matbout@users.noreply.github.com>
2024-06-08 11:38:44 -0400

    Add contract wrapper class to export to JS

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate napi_derive;
 
 use std::fs;
 
-use napi::bindgen_prelude::Buffer;
+use napi::bindgen_prelude::{BigInt, Buffer};
 use wasmer::Value;
 use wasmer_types::RawValue;
 
@@ -24,26 +24,92 @@ pub fn test(contract_address: String, deployer_address: String) {
     let _ = contract.call("getContract", &[]);
 }
 
-#[napi]
-pub fn call_test_1(function: String) -> i32 {
-    let contract_bytecode = fs::read(CONTRACT_PATH).expect("Unable to read contract file");
-    let mut contract = Contract::new(contract_bytecode, "bcrt1py2dhdrrf4s72gau3mkdw0mpnkgzp63qfdc0j7nah3luhmfcwf8kq4r44ef", "bcrt1pqdekymf30t583r8r9q95jyrgvyxcgrprajmyc9q8twae7ec275kq85vsev");
-    let _ = contract.init();
-    let response = contract.call(&function, &[]);
-    let boxed = response.unwrap();
-    let value: Value = boxed[0].clone();
-    value.unwrap_i32()
+
+#[napi(js_name = "ContractAPI")]
+struct ContractAPI {
+    contract: Contract,
 }
 
-#[napi]
-pub fn call_test_2(function: String, data: Buffer) {
-    let contract_bytecode = fs::read(CONTRACT_PATH).expect("Unable to read contract file");
-    let mut contract = Contract::new(contract_bytecode, "bcrt1py2dhdrrf4s72gau3mkdw0mpnkgzp63qfdc0j7nah3luhmfcwf8kq4r44ef", "bcrt1pqdekymf30t583r8r9q95jyrgvyxcgrprajmyc9q8twae7ec275kq85vsev");
-    let _ = contract.init();
+#[napi] //noinspection RsCompileErrorMacro
+impl ContractAPI {
+    #[napi(constructor)]
+    pub fn new(contract_address: String, deployer_address: String) -> Self {
+        let contract_bytecode = fs::read(CONTRACT_PATH).expect("Unable to read contract file");
+        let contract = Contract::new(contract_bytecode, &contract_address, &deployer_address);
+        Self { contract }
+    }
 
-    let bytes: Vec<u8> = data.into();
-    let chunks = bytes.chunks(16);
-    let raw_values: Vec<RawValue> = chunks.map(|chunk| RawValue { bytes: <[u8; 16]>::try_from(chunk).unwrap() }).collect();
+    #[napi]
+    pub fn init(&mut self) {
+        self.contract.init().unwrap();
+    }
 
-    let _ = contract.call_raw(&function, raw_values);
+    #[napi(js_name = "__new")]
+    pub fn __new(&mut self, size: i32, id: i32) -> i32 {
+        let __new = self.contract.instance.exports.get_function("__new").unwrap();
+        let response = __new.call(&mut self.contract.store, &[Value::I32(size), Value::I32(id)]);
+        let boxed = response.unwrap();
+        let value: Value = boxed[0].clone();
+        value.unwrap_i32()
+    }
+
+    #[napi(js_name = "__pin")]
+    pub fn __pin(&mut self, ptr: i32) -> i32 {
+        let __new = self.contract.instance.exports.get_function("__new").unwrap();
+        let response = __new.call(&mut self.contract.store, &[Value::I32(ptr)]);
+        let boxed = response.unwrap();
+        let value: Value = boxed[0].clone();
+        value.unwrap_i32()
+    }
+
+    #[napi]
+    pub fn call_test_1(&mut self, func_name: String) -> i32 {
+        let response = self.contract.call(&func_name, &[]);
+        let boxed = response.unwrap();
+        let value: Value = boxed[0].clone();
+        value.unwrap_i32()
+    }
+
+    #[napi]
+    pub fn call_test_2(&mut self, func_name: String, data: Buffer) {
+        let bytes: Vec<u8> = data.into();
+        let chunks = bytes.chunks(16);
+        // let raw_values: Vec<RawValue> = chunks
+        //     .map(|chunk| RawValue {
+        //         bytes: <[u8; 16]>::try_from(chunk),
+        //     })
+        //     .collect();
+
+        let mut raw_values: Vec<RawValue> = Vec::new();
+        for chunk in chunks {
+            if chunk.len() < 16 {
+                let mut padded_chunk = vec![0; 16];
+                padded_chunk[..chunk.len()].copy_from_slice(chunk);
+                raw_values.push(RawValue {
+                    bytes: padded_chunk.try_into().unwrap(),
+                });
+            } else {
+                raw_values.push(RawValue {
+                    bytes: chunk.try_into().unwrap(),
+                });
+            }
+        }
+
+        self.contract.call_raw(&func_name, raw_values).unwrap();
+    }
+
+    #[napi]
+    pub fn read_memory(&self, offset: BigInt, length: BigInt) -> Buffer {
+        let memory = self.contract.instance.exports.get_memory("memory").unwrap();
+        let mut buf: Vec<u8> = vec![0; length.get_u64().1 as usize];
+        memory.view(&self.contract.store).read(offset.get_u64().1, &mut buf).unwrap();
+        Buffer::from(buf)
+    }
+
+    #[napi]
+    pub fn write_memory(&self, offset: BigInt, data: Buffer) {
+        let data: Vec<u8> = data.into();
+        let memory = self.contract.instance.exports.get_memory("memory").unwrap();
+        memory.view(&self.contract.store).write(offset.get_u64().1, &data).unwrap();
+    }
 }
