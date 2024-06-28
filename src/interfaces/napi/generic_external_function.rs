88d42b39617d3b0a800f5e8a6c65568a1028fc4f
matbout <48333440+matbout@users.noreply.github.com>
2024-06-27 20:27:37 -0400

    Remove code duplication in external function objects

---
+++ src/interfaces/napi/generic_external_function.rs
@@ -0,0 +1,48 @@
+use napi::bindgen_prelude::{Buffer, Promise};
+use napi::threadsafe_function::{ErrorStrategy, ThreadsafeFunction};
+use tokio::runtime::Runtime;
+use wasmer::RuntimeError;
+
+use crate::interfaces::ExternalFunction;
+use crate::interfaces::napi::thread_safe_js_import_response::ThreadSafeJsImportResponse;
+
+pub struct GenericExternalFunction {
+    tsfn: ThreadsafeFunction<ThreadSafeJsImportResponse, ErrorStrategy::CalleeHandled>,
+}
+
+impl GenericExternalFunction {
+    pub fn new(
+        tsfn: ThreadsafeFunction<ThreadSafeJsImportResponse, ErrorStrategy::CalleeHandled>,
+    ) -> Self {
+        Self { tsfn }
+    }
+}
+
+impl ExternalFunction for GenericExternalFunction {
+    fn execute(&self, data: &[u8]) -> Result<Vec<u8>, RuntimeError> {
+        let request = ThreadSafeJsImportResponse {
+            buffer: Vec::from(data),
+        };
+
+        let deploy = async move {
+            let response: Result<Promise<Buffer>, RuntimeError> = self
+                .tsfn
+                .call_async(Ok(request))
+                .await
+                .map_err(|_e| RuntimeError::new("Error calling load function"));
+
+            let promise = response?;
+
+            let data = promise
+                .await
+                .map_err(|_e| RuntimeError::new("Error awaiting promise"))?;
+
+            let data = data.to_vec();
+            Ok(data.into())
+        };
+
+        let rt = Runtime::new().unwrap();
+        let response = rt.block_on(deploy);
+        response
+    }
+}
--- src/interfaces/napi/storage_load_external_function.rs
+++ src/interfaces/napi/storage_load_external_function.rs
@@ -1,48 +1,26 @@
-use napi::bindgen_prelude::{Buffer, Promise};
 use napi::threadsafe_function::{ErrorStrategy, ThreadsafeFunction};
-use tokio::runtime::Runtime;
 use wasmer::RuntimeError;
 
 use crate::interfaces::ExternalFunction;
+use crate::interfaces::napi::generic_external_function::GenericExternalFunction;
 use crate::interfaces::napi::thread_safe_js_import_response::ThreadSafeJsImportResponse;
 
 pub struct StorageLoadExternalFunction {
-    tsfn: ThreadsafeFunction<ThreadSafeJsImportResponse, ErrorStrategy::CalleeHandled>,
+    external_function: GenericExternalFunction,
 }
 
 impl StorageLoadExternalFunction {
     pub fn new(
         tsfn: ThreadsafeFunction<ThreadSafeJsImportResponse, ErrorStrategy::CalleeHandled>,
     ) -> Self {
-        Self { tsfn }
+        Self {
+            external_function: GenericExternalFunction::new(tsfn),
+        }
     }
 }
 
 impl ExternalFunction for StorageLoadExternalFunction {
     fn execute(&self, data: &[u8]) -> Result<Vec<u8>, RuntimeError> {
-        let request: ThreadSafeJsImportResponse = ThreadSafeJsImportResponse {
-            buffer: Vec::from(data),
-        };
-
-        let deploy = async move {
-            let response: Result<Promise<Buffer>, RuntimeError> = self
-                .tsfn
-                .call_async(Ok(request))
-                .await
-                .map_err(|_e| RuntimeError::new("Error calling load function"));
-
-            let promise = response?;
-
-            let data = promise
-                .await
-                .map_err(|_e| RuntimeError::new("Error awaiting promise"))?;
-
-            let data = data.to_vec();
-            Ok(data.into())
-        };
-
-        let rt: Runtime = Runtime::new().unwrap();
-        let response = rt.block_on(deploy);
-        response
+        self.external_function.execute(data)
     }
 }
