d61ca21b53893e73bb4d521f10e36f0d6ec8dbd1
matbout <48333440+matbout@users.noreply.github.com>
2024-07-21 20:53:33 -0400

    Rename op_costs to gas_costs

--- src/domain/vm/mod.rs
+++ src/domain/vm/mod.rs
@@ -1,7 +1,7 @@
+pub use self::gas_costs::*;
 pub use self::limiting_tunables::*;
 pub use self::logger::*;
-pub use self::op_costs::*;
 
-mod op_costs;
+mod gas_costs;
 mod limiting_tunables;
 mod logger;
