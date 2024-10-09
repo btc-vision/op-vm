bd7c1c62793ad57ce1512bebb87ce79e10fcebfa
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-10-08 22:44:09 -0400

    Gas parameters

--- src/domain/runner/constants/constants.rs
+++ src/domain/runner/constants/constants.rs
@@ -5,13 +5,17 @@ pub const PAGE_MEMORY_SIZE: u64 = 64 * 1024;
 pub const MAX_MEMORY_SIZE: u64 = (MAX_PAGES as u64) * PAGE_MEMORY_SIZE;
 
 /** Gas cost for custom functions */
-pub const LOAD_COST: u64 = 21_000_000;
-pub const STORE_COST: u64 = 221_000_000;
+pub const LOAD_COST: u64 = 20_000_000;
+pub const STORE_COST: u64 = 140_000_000;
+
 pub const CALL_COST: u64 = 343_000_000;
 pub const DEPLOY_COST: u64 = 2_500_000_000;
-pub const ENCODE_ADDRESS_COST: u64 = 4_000_000;
-pub const SHA256_COST: u64 = 1_000_000;
-pub const RIMD160_COST: u64 = 1_000_000;
-pub const INPUTS_COST: u64 = 500_000_000;
-pub const OUTPUTS_COST: u64 = 500_000_000;
+pub const ENCODE_ADDRESS_COST: u64 = 1_000_000;
+
+pub const SHA256_COST: u64 = 50_000;
+pub const RIMD160_COST: u64 = 50_000;
+
+pub const INPUTS_COST: u64 = 5_000_000;
+pub const OUTPUTS_COST: u64 = 5_000_000;
+
 pub const EMIT_COST: u64 = 1_000_000;
