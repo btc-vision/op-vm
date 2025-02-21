4ff104c2daf66f0d9a54842a814c0ecfea1215fb
matbout <48333440+matbout@users.noreply.github.com>
2025-02-21 19:14:50 +0400

    Simplify data format for deploy import

--- src/domain/runner/import_functions/deploy_from_address_import.rs
+++ src/domain/runner/import_functions/deploy_from_address_import.rs
@@ -1,4 +1,3 @@
-use crate::domain::assembly_script::AssemblyScript;
 use crate::domain::runner::CustomEnv;
 use crate::interfaces::ExternalFunction;
 use wasmer::{FunctionEnvMut, RuntimeError};
@@ -9,23 +8,46 @@ pub const STATIC_GAS_COST: u64 = 2_500_000_000;
 pub struct DeployFromAddressImport;
 
 impl DeployFromAddressImport {
-    pub fn execute(mut context: FunctionEnvMut<CustomEnv>, ptr: u32) -> Result<u32, RuntimeError> {
+    pub fn execute(
+        mut context: FunctionEnvMut<CustomEnv>,
+        origin_address_ptr: u32,
+        salt_ptr: u32,
+        result_address_ptr: u32,
+    ) -> Result<u32, RuntimeError> {
         let (env, mut store) = context.data_and_store_mut();
         let instance = env
             .instance
             .clone()
             .ok_or(RuntimeError::new("Instance not found"))?;
 
-        let data = AssemblyScript::read_buffer(&mut store, &instance, ptr)
-            .map_err(|_e| RuntimeError::new("Error lifting typed array"))?;
+        let origin_address = instance
+            .read_memory(&store, origin_address_ptr as u64, 32)
+            .map_err(|_e| RuntimeError::new("Error reading address from memory"))?;
+        let salt = instance
+            .read_memory(&store, salt_ptr as u64, 32)
+            .map_err(|_e| RuntimeError::new("Error reading salt from memory"))?;
+        let data = [origin_address.as_slice(), salt.as_slice()].concat();
 
         instance.use_gas(&mut store, STATIC_GAS_COST);
+
         let result = &env
             .deploy_from_address_external
             .execute(&data, &env.runtime)?;
-        let value = AssemblyScript::write_buffer(&mut store, &instance, &result, 13, 0)
-            .map_err(|e| RuntimeError::new(format!("Error writing buffer: {}", e)))?;
 
-        Ok(value as u32)
+        let result_address = result.get(0..32).ok_or(RuntimeError::new(
+            "Invalid data received for 'Deploy from address'",
+        ))?;
+        // let bytecode_length = result.get(32..36).ok_or(RuntimeError::new(
+        //     "Invalid data received for 'Deploy from address'",
+        // ))?;
+        instance
+            .write_memory(&store, result_address_ptr as u64, result_address)
+            .map_err(|_e| RuntimeError::new("Error writing contract address to memory"))?;
+
+        if result_address == [0; 32] {
+            return Ok(1);
+        }
+
+        Ok(0)
     }
 }
