f56bd1010bb621014fb408938e039410d58035dc
matbout <48333440+matbout@users.noreply.github.com>
2025-02-22 15:57:18 +0400

    Simplify data format for verify Schnorr import

--- src/domain/runner/import_functions/verify_schnorr_import.rs
+++ src/domain/runner/import_functions/verify_schnorr_import.rs
@@ -1,17 +1,20 @@
-use crate::domain::assembly_script::AssemblyScript;
 use crate::domain::runner::CustomEnv;
 use once_cell::sync::Lazy;
 use secp256k1::{schnorr, Secp256k1, XOnlyPublicKey};
 use wasmer::{FunctionEnvMut, RuntimeError};
 
 pub const STATIC_GAS_COST: u64 = 100_000_000;
-pub const GAS_COST_PER_WORD: u64 = 120_000;
 
 #[derive(Default)]
 pub struct VerifySchnorrImport;
 
 impl VerifySchnorrImport {
-    pub fn execute(mut context: FunctionEnvMut<CustomEnv>, ptr: u32) -> Result<u32, RuntimeError> {
+    pub fn execute(
+        mut context: FunctionEnvMut<CustomEnv>,
+        public_key_ptr: u32,
+        signature_ptr: u32,
+        message_ptr: u32,
+    ) -> Result<u32, RuntimeError> {
         static SECP: Lazy<Secp256k1<secp256k1::All>> = Lazy::new(|| Secp256k1::new());
 
         let (env, mut store) = context.data_and_store_mut();
@@ -21,42 +24,31 @@ impl VerifySchnorrImport {
             .clone()
             .ok_or(RuntimeError::new("Instance not found"))?;
 
-        let data = AssemblyScript::read_buffer(&store, &instance, ptr)
-            .map_err(|_e| RuntimeError::new("Error lifting typed array"))?;
-
-        let public_key_bytes = data.get(0..32).ok_or(RuntimeError::new("Invalid buffer"))?;
-        let signature_bytes = data
-            .get(32..96)
-            .ok_or(RuntimeError::new("Invalid signature"))?;
-        let message_bytes = data
-            .get(96..128)
-            .ok_or(RuntimeError::new("Invalid message"))?;
-
-        let pub_key_bytes: [u8; 32] = public_key_bytes
+        let public_key_bytes: [u8; 32] = instance
+            .read_memory(&store, public_key_ptr as u64, 32)
+            .map_err(|_e| RuntimeError::new("Error reading Schnorr public key from memory"))?
             .try_into()
-            .map_err(|e| RuntimeError::new(format!("Error converting bytes: {}", e)))?;
+            .map_err(|_e| RuntimeError::new("Error reading Schnorr public key from memory"))?;
 
-        let signature_bytes: [u8; 64] = signature_bytes
+        let signature_bytes: [u8; 64] = instance
+            .read_memory(&store, signature_ptr as u64, 64)
+            .map_err(|_e| RuntimeError::new("Error reading Schnorr signature from memory"))?
             .try_into()
-            .map_err(|e| RuntimeError::new(format!("Error converting bytes: {}", e)))?;
+            .map_err(|_e| RuntimeError::new("Error reading Schnorr signature from memory"))?;
 
-        let xonly_public_key = XOnlyPublicKey::from_byte_array(&pub_key_bytes)
+        let message_bytes = instance
+            .read_memory(&store, message_ptr as u64, 32)
+            .map_err(|_e| RuntimeError::new("Error reading Schnorr message from memory"))?;
+
+        let xonly_public_key = XOnlyPublicKey::from_byte_array(&public_key_bytes)
             .map_err(|e| RuntimeError::new(format!("Error converting public key: {}", e)))?;
 
         let signature = schnorr::Signature::from_byte_array(signature_bytes);
-        let valid = SECP.verify_schnorr(&signature, &message_bytes, &xonly_public_key);
+        let result = SECP.verify_schnorr(&signature, &message_bytes, &xonly_public_key);
+        let result = result.is_ok() as u32;
 
-        let result = if valid.is_ok() { vec![1] } else { vec![0] };
+        instance.use_gas(&mut store, STATIC_GAS_COST);
 
-        let value = AssemblyScript::write_buffer(&mut store, &instance, &result, 13, 0)
-            .map_err(|e| RuntimeError::new(format!("Error writing buffer: {}", e)))?;
-
-        instance.use_gas(
-            &mut store,
-            STATIC_GAS_COST
-                + ((data.len() + 31) / 32) as u64 * GAS_COST_PER_WORD,
-        );
-
-        Ok(value as u32)
+        Ok(result)
     }
 }
