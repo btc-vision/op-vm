3a0a536a755fd8b91d8d10e2f0fe96e03728ace0
matbout <48333440+matbout@users.noreply.github.com>
2025-03-30 21:16:15 +0400

    Update gas costs

--- src/domain/runner/import_functions/block_hash_import.rs
+++ src/domain/runner/import_functions/block_hash_import.rs
@@ -1,7 +1,7 @@
 use crate::domain::runner::CustomEnv;
 use wasmer::{FunctionEnvMut, RuntimeError};
 
-const STATIC_GAS_COST: u64 = 1_000_000;
+const STATIC_GAS_COST: u64 = 21_000_000;
 
 #[derive(Default)]
 pub struct GetBlockHashImport;
