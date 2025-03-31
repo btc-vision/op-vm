a6531635833a2d95c1efe30ad092885a269337cf
matbout <48333440+matbout@users.noreply.github.com>
2025-03-31 14:34:48 +0400

    Change napi type

--- src/interfaces/napi/js_contract_manager.rs
+++ src/interfaces/napi/js_contract_manager.rs
@@ -100,7 +100,7 @@ impl ContractManager {
             ts_arg_type = "(_: never, result: ThreadSafeJsImportResponse) => Promise<Buffer | Uint8Array>"
         )]
         outputs_js_function: JsFunction,
-        #[napi(ts_arg_type = "(_: never, result: ThreadSafeJsImportResponse) => Promise<Int>")]
+        #[napi(ts_arg_type = "(_: never, result: ThreadSafeJsImportResponse) => Promise<number>")]
         account_type_js_function: JsFunction,
         #[napi(
             ts_arg_type = "(_: never, result: BlockHashRequest) => Promise<Buffer | Uint8Array>"
