7d4f13cd9e83a2cded2e3ff3f5e5b2841b0c6fcf
matbout <48333440+matbout@users.noreply.github.com>
2025-04-03 17:34:11 +0400

    Update comments in constants

--- src/domain/runner/constants/constants.rs
+++ src/domain/runner/constants/constants.rs
@@ -1,5 +1,5 @@
-pub const MAX_PAGES: u32 = 128 * 4; // 1 page = 64KB, 32 MB.
-pub const STACK_SIZE: usize = 1024 * 1024; // 1MB
+pub const MAX_PAGES: u32 = 512; // 1 page = 64 KiB, 32 MiB.
+pub const STACK_SIZE: usize = 1024 * 1024; // 1 MiB
 pub const PAGE_MEMORY_SIZE: u64 = 64 * 1024;
 pub const MAX_MEMORY_SIZE: u64 = (MAX_PAGES as u64) * PAGE_MEMORY_SIZE;
 pub const MAX_GAS_WASM_INIT: u64 = 20_000_000;
