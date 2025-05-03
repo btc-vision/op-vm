71ba27ce3d49307485cd41513949a209a98c1111
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-05-03 18:32:33 -0400

    Added missing exports

--- test/index.js
+++ test/index.js
@@ -1,5 +1,25 @@
 import { ContractManager } from '../index.js';
 
-const test = new ContractManager();
+const test = new ContractManager(16, () => {
+    console.log('storage_load_js_function');
+}, () => {
+    console.log('storage_store_js_function');
+}, () => {
+    console.log('call_other_contract_js_function');
+}, () => {
+    console.log('deploy_from_address_js_function');
+}, () => {
+    console.log('console_log_js_function');
+}, () => {
+    console.log('emit_js_function');
+}, () => {
+    console.log('inputs_js_function');
+}, () => {
+    console.log('outputs_js_function');
+}, () => {
+    console.log('account_type_js_function');
+}, () => {
+    console.log('block_hash_js_function');
+});
 
-console.log(test);
+console.log('from prototype:', test.reserveId);
