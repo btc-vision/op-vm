59e904f20bf34c1185704202cd70c3598fd0b286
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-05-03 18:16:09 -0400

    Fixes

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-const native = require('./index.node');
-const BitcoinNetworkRequest = Object.freeze({
-    Mainnet: 0,
-    Testnet: 1,
-    Regtest: 2
-  });
+import { createRequire } from 'module';
 
-const EnvironmentVariablesRequest = {
-    blockHash: new Uint8Array(),
-    blockNumber: 0n,
-    blockMedianTime: 0n,
-    txId: new Uint8Array(),
-    txHash: new Uint8Array(),
-    contractAddress: new Uint8Array(),
-    contractDeployer: new Uint8Array(),
-    caller: new Uint8Array(),
-    origin: new Uint8Array()
-};
+const require = createRequire(import.meta.url);
+const nativeModule = require('./index.node');
 
-module.exports = {
-    BitcoinNetworkRequest: BitcoinNetworkRequest,
-    EnvironmentVariablesRequest: EnvironmentVariablesRequest,
-    ContractManager: native.ContractManager,
-    NEW_STORAGE_SLOT_GAS_COST: native.NEW_STORAGE_SLOT_GAS_COST,
-    UPDATED_STORAGE_SLOT_GAS_COST: native.UPDATED_STORAGE_SLOT_GAS_COST
-};
-
-console.log(module.exports)
-console.log(module.exports)
+export default nativeModule;
---
+++ test/index.js
@@ -0,0 +1,15 @@
+import native from '../index.js';
+
+console.log(native);
+
+/*a.test(1n, {
+    blockHash: new Uint8Array(32),
+    blockNumber: 10n,
+    blockMedianTime: 100n,
+    txId: new Uint8Array(32),
+    txHash: new Uint8Array(32),
+    contractAddress: new Uint8Array(32),
+    contractDeployer: new Uint8Array(32),
+    caller: new Uint8Array(32),
+    origin: new Uint8Array(32),
+});*/
