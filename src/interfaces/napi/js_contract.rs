2ab936ba571e7d3a4b86703a0a9ced4cdcb37f12
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-05-18 22:26:24 -0400

    Update js_contract.rs

--- src/interfaces/napi/js_contract.rs
+++ src/interfaces/napi/js_contract.rs
@@ -177,12 +177,7 @@ impl JsContract {
 
             // TODO: LEAVE LIKE THIS FOR NOW, SEE IF WE CAN STOP THE ERROR: ERROR: ERROR EVENTUALLY
             // FIXED THE MASSIVE ERROR OVERFLOW OF ERROR USING ROOT_CAUSE
-            Err(e) => {
-                //println!("Error: {:?}", e);
-                //println!("Root cause: {:?}", e.root_cause());
-
-                Err(Error::from_reason(e.root_cause().to_string()))
-            }
+            Err(e) => Err(Error::from_reason(e.root_cause().to_string())),
         };
 
         log_time_diff(&time, "JsContract::execute");
